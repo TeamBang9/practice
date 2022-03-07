13460c8da517cd8ccba3cd1d37a3adb9ccb62f64
ignim94 <chlaksrk13@gmail.com>
2022-03-07 17:50:00 +0900

    week-d2

--- 정규표현식/week02/mingi.java
+++ 정규표현식/week02/mingi.java
@@ -2,7 +2,7 @@ public class Gohome {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- /*
+		/*
 		1. **Backreferences To Failed Groups**
 
 		→tester.checker("^((\\d{8})|(\\d{2}-\\d{2}-\\d{2}-\\d{2}))$");

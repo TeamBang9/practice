5821569c2f333c189548037ebd1c6ec509b7f37f
ignim94 <chlaksrk13@gmail.com>
2022-03-07 13:39:40 +0900

    mingi_week2

---
+++ 정규표현식/week02/mingi.java
@@ -0,0 +1,15 @@
+public class Gohome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ /*
+		1. **Backreferences To Failed Groups**
+
+		→tester.checker("^((\\d{8})|(\\d{2}-\\d{2}-\\d{2}-\\d{2}))$");
+		
+		2. **Forward References**
+		
+		→tester.checker("^(\\2tic|(tac))*$"); // Use \\ instead of using \
+*/
+	}
+}

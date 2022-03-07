0d8e1055f6bdb8a49528f925feb79a069c27d8c4
ignim94 <chlaksrk13@gmail.com>
2022-03-07 15:46:32 +0900

    mingi

--- 정규표현식/week02/mingi.java
+++ 정규표현식/week02/mingi.java
@@ -9,7 +9,13 @@ public class Gohome {
 		
 		2. **Forward References**
 		
-		→tester.checker("^(\\2tic|(tac))*$"); // Use \\ instead of using \
-*/
+		→ tester.checker("^(\\2tic|(tac))*$"); // Use \\ instead of using \
+
+		#1. Branch Reset Groups -> 해당 언어가 없어서 스킵
+
+		#3. Matching Same Text Again & Again
+		->tester.checker("^([a-z]\\w\\s\\W\\d\\D[A-Z][a-zA-Z][aeiouAEIOU]\\S)\\1$"); // Use \\ instead of using \
+
+		*/
 	}
 }
